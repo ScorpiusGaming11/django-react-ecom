68bd60a78658b4593a0585d603c2044b694c1c05
ScorpiusGaming11 <kishenthiren7@gmail.com>
2025-03-26 09:55:14 +0800

    Completed Home and About page, and added Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -3,23 +3,23 @@ import './About.css';
 
 const About = () => {
   return (
-    <div className="container about">
-      <div className="row align-items-center mb-5">
+    <div className="about d-flex flex-column justify-content-center align-items-center">
+      <div className="about-section-1 row align-items-center">
         <div className="col-md-6 about-1-img">
-          <img src="../../assets/about-1.jpg" alt="About Image" className="img-fluid" />
+          <img src="/assets/about-1.jpg" alt="About Image" className="img-fluid rounded shadow-lg" />
         </div>
         <div className="col-md-6 about-1-texts">
-          <h1>Who Are We?</h1>
-          <p>We are a passionate team dedicated to bringing the beauty of nature into your homes. We believe in the power of plants to enhance your living spaces and improve your well-being.</p>
+          <h1 className="display-3 fw-semibold">Who Are We?</h1>
+          <p>We are a passionate team of plant enthusiasts dedicated to bringing the beauty and tranquility of nature into your homes. Our journey began with a simple love for greenery and a desire to create living spaces that inspire and rejuvenate. We believe that plants have the power to transform any environment, fostering a sense of calm and well-being. With years of experience in horticulture and a deep understanding of plant care, we curate a diverse selection of high-quality plants, each chosen for its unique beauty and ability to thrive in various settings.</p>
         </div>
       </div>
-      <div className="row align-items-center">
+      <div className=" about-section-2 row align-items-center">
         <div className="col-md-6 about-2-texts order-md-1">
-          <h1>What's Our Mission?</h1>
-          <p>Our mission is to provide high-quality plants and exceptional customer service. We strive to make gardening accessible to everyone, from beginners to experienced plant enthusiasts.</p>
+          <h1 className="display-3 fw-semibold">What's Our Mission?</h1>
+          <p>Our mission is to provide high-quality plants and exceptional customer service, fostering a thriving community of plant lovers. We strive to make gardening accessible to everyone, from beginners taking their first steps into gardening to experienced plant enthusiasts seeking to expand their collections. Through our carefully curated selection of healthy plants, expert advice, and dedication to customer satisfaction, we empower our customers to create beautiful and fulfilling green spaces that enhance their lives and contribute to a healthier planet.</p>
         </div>
         <div className="col-md-6 about-2-img order-md-2">
-          <img src="../../assets/about-2.jpg" alt="About Image" className="img-fluid" />
+          <img src="/assets/about-2.jpg" alt="About Image" className="img-fluid rounded shadow-lg" />
         </div>
       </div>
     </div>
